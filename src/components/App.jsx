ce688419d0b17ffb155219a53becc1d9560c33ae
ramocoblisan <ramonaioana67@yahoo.com>
2024-04-21 19:41:58 +0300

    App.jsx updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import Button from "./Button/Button";
 import ImageGallery from "./ImageGallery/ImageGallery";
-import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
 import Loader from "./Loader/Loader";
 import Modal from "./Modal/Modal";
 import Searchbar from "./Searchbar/Searchbar";
 import { config } from "./data/config";
-
 
 export class App extends Component {
   constructor (props){
